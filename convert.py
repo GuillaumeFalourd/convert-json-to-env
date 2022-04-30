c17d820834d71f9e83b5476a9f7ce53ddc214671
GuillaumeFalourd <guillaume.falourd@gmail.com>
2022-04-30 11:12:19 -0300

    OS action tests
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@gmail.com>

--- convert.py
+++ convert.py
@@ -10,12 +10,12 @@ filename = sys.argv[1]
 # Opening JSON file
 f = open(filename)
  
-# returns JSON object as a dictionary
+# Returns JSON object as a dictionary
 data = json.load(f)
 
+# Add Key value to GITHUB_ENV (key will be UPPER CASE)
 for key, value in data.items():
     key = key.upper()
-    print(key, value)
     with open(env_file, "a") as myfile:
         myfile.write(f"{key}={value}" + '\n')
 
