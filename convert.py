e95c9e5fccda430995f3434aae508742a16e00e6
GuillaumeFalourd <guillaume.falourd@gmail.com>
2022-04-30 10:55:20 -0300

    update action
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@gmail.com>

---
+++ convert.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+print("Start conversion")
+print(sys.argv[1:])
+filename = sys.argv[1:]
+
+# Opening JSON file
+f = open(filename)
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+ 
+# Iterating through the json
+# list
+print(data)
+ 
+# Closing file
+f.close()
+
+print("End conversion")
