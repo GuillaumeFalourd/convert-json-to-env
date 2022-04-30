ea86789b9aa4ea38bd80cfdb75b5e40348e72ff8
GuillaumeFalourd <guillaume.falourd@gmail.com>
2022-04-30 11:07:14 -0300

    update action
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@gmail.com>

--- convert.py
+++ convert.py
@@ -1,21 +1,24 @@
 import json
 import sys
+import os
+
+env_file = os.getenv('GITHUB_ENV')
 
 print("Start conversion")
-print(sys.argv[1:])
 filename = sys.argv[1]
 
 # Opening JSON file
 f = open(filename)
  
-# returns JSON object as
-# a dictionary
+# returns JSON object as a dictionary
 data = json.load(f)
- 
-# Iterating through the json
-# list
-print(data)
- 
+
+for key, value in data.items():
+    key = key.upper()
+    print key, value
+    with open(env_file, "a") as myfile:
+    myfile.write(f"{key}={value}")
+
 # Closing file
 f.close()
 
