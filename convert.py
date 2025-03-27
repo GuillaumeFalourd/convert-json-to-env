8ea9d601d2fabe9bb02e6ab584737cee962f10e5
Max Rakitin <mrakitin@bnl.gov>
2025-03-26 21:43:58 -0400

    Use `with` context for opening the input file

--- convert.py
+++ convert.py
@@ -2,24 +2,20 @@ import json
 import sys
 import os
 
-env_file = os.getenv('GITHUB_ENV')
+env_file = os.getenv("GITHUB_ENV")
 
 print("Start conversion")
 filename = sys.argv[1]
 
 # Opening JSON file
-f = open(filename)
- 
-# Returns JSON object as a dictionary
-data = json.load(f)
+with open(filename, "r") as f:
+    # Returns JSON object as a dictionary
+    data = json.load(f)
 
 # Add Key value to GITHUB_ENV (key will be UPPER CASE)
 for key, value in data.items():
     key = key.upper()
     with open(env_file, "a") as myfile:
         myfile.write(f"{key}={value}" + '\n')
-
-# Closing file
-f.close()
 
 print("End conversion")
